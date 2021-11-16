6c70610ca7f0c561b56884e6c6999d1d36a69973
Rafael Álvarez <rafaelalma92@gmail.com>
2021-11-16 21:39:52 +0100

    Set up modules and factories

---
+++ main.js
@@ -0,0 +1,16 @@
+const GameBoard = (() => {
+  const gameBoard = [];
+
+  return {};
+})();
+
+const DisplayController = (() => {
+  return {};
+})();
+
+const Player = () => {
+  return {};
+};
+
+const player1 = Player();
+const player2 = Player();
